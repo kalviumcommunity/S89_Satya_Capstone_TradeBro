c4632a00d9ea0648ed63424ccbba579c47e2fda0
Satya-s89 <kaki.harisatya.s89@kalvium.community>
2025-04-15 10:56:02 +0530

    SCHEMA

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const apiRoutes = require("./routes/apiRoutes");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use("/api", apiRoutes);
+
+mongoose 
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 app.listen(5000, () => console.log("Server running on port 5000"));
