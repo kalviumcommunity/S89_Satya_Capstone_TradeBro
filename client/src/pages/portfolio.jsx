8b12c65d4b31fd094af17d98ccdf608693f4944c
Satya-s89 <kaki.harisatya.s89@kalvium.community>
2025-05-22 11:33:07 +0530

    deploy

--- client/src/pages/portfolio.jsx
+++ client/src/pages/portfolio.jsx
@@ -16,7 +16,7 @@ import FullScreenStockDetail from "../components/FullScreenStockDetail";
 import StockSearch from "../components/StockSearch";
 import axios from "axios";
 import API_ENDPOINTS from "../config/apiConfig";
-import "../styles/pages/Portfolio.css";
+import "../styles/pages/portfolio.css";
 
 // Add some additional styles for the clickable rows
 const additionalStyles = `
