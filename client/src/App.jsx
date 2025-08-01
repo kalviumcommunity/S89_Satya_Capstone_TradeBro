0b1ab6504107bd4ff04010a88c922e85d05235e3
Satya-s89 <kaki.harisatya.s89@kalvium.community>
2025-08-01 21:01:46 +0530

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ import useGlobalSearch from './hooks/useGlobalSearch'
 import { useOrderIntegration } from './hooks/useOrderIntegration'
 import { usePerformanceOptimization } from './hooks/usePerformanceOptimization'
 import balanceSyncManager from './utils/balanceSync'
-import AppRoutes from './AppRoutes.jsx'
+import AppRoutes from './Approutes'
 
 const PerformanceMonitor = lazy(() => import('./components/debug/PerformanceMonitor'))
 
